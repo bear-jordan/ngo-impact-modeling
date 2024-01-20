0ae1fa2311db660d102b2f9d362c846e9013ebea
Bear Jordan <bearjordan@gmail.com>
2024-01-20 22:37:46 +1300

    added upload

--- app/src/mecklenburg/main.py
+++ app/src/mecklenburg/main.py
@@ -1,13 +1,27 @@
+from datetime import datetime
 from pathlib import Path
 from yaml import safe_load
 
+from boto import client
 from icecream import ic
 
 from mecklenburg.data import gen_data
 from mecklenburg.model import gen_fit
 
 
+AWS_CFG_PATH = "./mecklenburg/config/aws.yaml"
 CFG_PATH = "./mecklenburg/config/gen-data.yaml"
+
+
+def upload_to_s3(data):
+    with Path(AWS_CFG_PATH).open("r") as file:
+        aws_cfg = safe_load(file)
+
+    bucket = aws_cfg["bucket"]
+    current_time = datetime.now().strftime('%Y-%m-%d-%H:%M:%S')
+    name = f"results_{current_time}.json"
+    s3_client = client("s3")
+    s3_client.upload_file(data, bucket, name)
 
 
 def main():
@@ -26,6 +40,7 @@ def main():
             })
 
     ic(results)
+    upload_to_s3(results)
 
 
 if __name__ == "__main__":
